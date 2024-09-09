0fd2f18c50c5475c78f9a32ebffea42aa046fb74
Clement MARCEL <cmarcel@millesima.com>
2024-09-09 16:29:34 +0200

    test projects css

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
+
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField(upload_to='projects/')
+    image = models.ImageField(upload_to='portfolio/static/projects/')
     link = models.URLField()
