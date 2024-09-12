a7ba98e95fff9faff0a795d7b626bd410cb3a470
Clement MARCEL <lecramproject@gmail.com>
2024-09-12 20:02:35 +0200

    add whitenoise'

--- app/settings.py
+++ app/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django_browser_reload.middleware.BrowserReloadMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "app.urls"
