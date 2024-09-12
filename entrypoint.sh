b7fda21fe196839396c935367d6605a5dacb5398
Clement MARCEL <lecramproject@gmail.com>
2024-09-12 19:46:44 +0200

    add entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+poetry install
+python manage.py makemigrations
+python manage.py migrate
+python manage.py tailwind install
+python manage.py tailwind build
+python manage.py collecstatic
+exec "$@"
