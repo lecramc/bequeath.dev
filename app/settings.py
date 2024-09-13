5b635f9cafe9d6c4967ecd0acd3f2cbb12aad0e0
Clement MARCEL <lecramproject@gmail.com>
2024-09-13 23:28:53 +0200

    new storage conf

--- app/settings.py
+++ app/settings.py
@@ -139,11 +139,15 @@ if USE_S3:
     # s3 static settings
     AWS_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     # s3 public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'app.storage_backends.PublicMediaStorage'
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.s3.S3Storage",
+
+        },
+    }
 else:
     STATIC_URL = "static/"
     STATIC_ROOT = "static/"
