0c752a74de75bf46e05f9b00e219d04c65fcc8a1
Clement MARCEL <cmarcel@millesima.com>
2024-09-13 11:51:24 +0200

    desavtivate __reload in prod mode

--- app/urls.py
+++ app/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
                   path("admin/", admin.site.urls),
                   path('', include("portfolio.urls")),
                   path('blog/', include("blog.urls")),
-                  path("__reload__/", include("django_browser_reload.urls")),
-
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [path("__reload__/", include("django_browser_reload.urls"))]
