14956ff99c37076701bf1f4da1686f697e31a236
Clement MARCEL <lecramproject@gmail.com>
2024-10-13 15:23:55 +0200

    update text:

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() in ['true', '1']
-print(DEBUG)
+
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
